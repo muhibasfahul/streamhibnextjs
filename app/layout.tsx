0077ed799eabb952ff38b7a791d911a3f06460b8
Asfahul Muhib <muhibasfahul@gmail.com>
2025-06-13 16:39:43 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         <LanguageProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
